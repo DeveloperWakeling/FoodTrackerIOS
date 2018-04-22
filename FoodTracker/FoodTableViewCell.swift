e920e9b6074ee62abd82365f17b5e2b33e046dfa
Michael Wakeling <michaelwakeling@Michaels-MacBook-Pro.local>
2018-04-22 17:24:44 -0400

    Table View initially started

---
+++ FoodTracker/FoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Michael Wakeling on 4/22/18.
+//  Copyright © 2018 Michael Wakeling. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
